bf50a41696c74430776d4e60f0acf270574d46fb
Viktor <viktorkote98@gmail.com>
2021-01-10 14:42:08 +0100

    create_entry only saves to json file

--- index.js
+++ index.js
@@ -28,15 +28,9 @@ app.use(cors({
     'preflightContinue': true
 }));
 
+
+
 app.get('/', (req, res) => {
-
-    res.json({
-        json: 0
-    })
-
-})
-
-app.get('/get_json_file', (req, res) => {
 
     jsonFile = jsonManipulator.readFile()
 
@@ -45,6 +39,10 @@ app.get('/get_json_file', (req, res) => {
             json: JSON.stringify(data)
         })
 
+    }).catch(err => {
+        res.json({
+            message: err
+        })
     })
 })
 
@@ -56,85 +54,35 @@ app.post('/create_entry', (req, res) => {
 
         res.status(400).send('Tag is required')
 
-
-
     }
     if (reqData.response == undefined || reqData.response == null || reqData.response == '') {
 
         res.status(400).send('Response is required')
-
-
 
     }
     if (reqData.pattern == undefined || reqData.pattern == null || reqData.pattern == '') {
 
         res.status(400).send('Response is required')
 
-
-
     }
     jsonFile = jsonManipulator.readFile()
 
-
-
     jsonFile.then(data => {
+
         return JSON.parse(data)
+
     }).then(parsedJson => {
-        if (reqData.context_filter != undefined && reqData.context_filter != null && reqData.context_filter != '') {
-            // find the elements with same context_set as the context_filter of the request,
-            // get their response and pattern and you append them, together and add them to the
-            // pattern of the new entry.
-            arr_context_set = parsedJson.intents.filter(obj => {
 
-                return obj.context_set == reqData.context_filter
+        parsedJson.intents.push(reqData)
+        return parsedJson
 
-
-            })
-
-            // console.log(arr_context_set)
-
-            new_pattern = [];
-            arr_context_set[0].patterns.forEach(pattern => {
-                arr_context_set[0].responses.forEach(response => {
-                    str = response + pattern;
-                    // console.log(str)
-                    new_pattern.push(str)
-                })
-            });
-            // console.log(new_pattern);
-
-            new_obj = {
-                tag: reqData.tag,
-                patterns: new_pattern,
-                responses: reqData.response,
-                context_set: reqData.context_set,
-                context_filter: reqData.context_filter
-
-            }
-            parsedJson.intents.push(new_obj)
-            return parsedJson
-        } else {
-
-            new_obj = {
-                tag: reqData.tag,
-                patterns: reqData.pattern,
-                responses: reqData.response,
-                context_set: reqData.context_set,
-            }
-
-            parsedJson.intents.push(new_obj)
-            return parsedJson
-        }
     }).then(writeJson => {
 
         jsonManipulator.writeFile(JSON.stringify(writeJson))
 
-
-
         res.status(200).json({
             message: 'Saved!'
         })
-
 
     }).catch(err => {
         if (!res.headersSent) {
@@ -144,8 +92,6 @@ app.post('/create_entry', (req, res) => {
         }
 
     })
-
-
 })
 
 // Go through all the elements and do the logic with context_set and context_filter
